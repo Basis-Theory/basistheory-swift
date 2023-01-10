5edd3376a7e96c737699ed8882610365db045de2
Brian Gonzalez <brian.gonzalez@basistheory.com>
2023-01-10 10:04:25 -0700

    chore: removes watchOS from platform list

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
         .iOS(.v9),
         .macOS(.v10_11),
         .tvOS(.v9),
-        .watchOS(.v3),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
